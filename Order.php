a4264e0d76b8e55cbbbe907c91a5c65ff221d7b1
anthony gevers <anthony.gevers@lapiscine.pro>
2024-11-07 12:45:43 +0100

    name

--- Order.php
+++ Order.php
@@ -1,41 +1,53 @@
 <?php
-//Je crée ma classe
+
+
 class Order {
-
-//J'ajoute mes propriétés
     public $id;
-
     public $customerName;
-
     public $status = "cart";
-
-    public $totalPrice = 0 ;
-
-    //Je met en place un tableau vide
+    public $totalPrice;
     public $products = [];
 
 
-    //Je crée une fonction qui permet d'ajouter les produits
+    // le constructeur est une méthode "magique"
+    // car elle est appelée automatiquement
+    // le constructeur est appelée quand un objet est créé
+    // pour cette classe
+    // un objet créé pour une classe est appelée "instance de class"
+    public function __construct($customerName) {
+        $this->customerName = $customerName;
+        $this->id = uniqid();
+    }
+
     public function addProduct() {
+        // le $this fait référence à l'objet actuel
+        // c'est à dire à $order1, ou $order2 etc
+        // donc à l'objet actuel issu de la classe
         if ($this->status === "cart") {
             $this->products[] = "Pringles";
             $this->totalPrice += 3;
         }
     }
 
-    //Je crée une fonction qui permet de payer les produits du "cart"
-    public function pay () {
+    public function pay() {
         if($this->status === "cart") {
             $this->status = "paid";
         }
     }
 }
-//Ici j'ajouter un produit a chaque "addProduct()
-$order1 = new Order();
-$order1->addProduct();
-$order1->addProduct();
-$order1->addProduct();
-$order1->addProduct();
-$order1->pay();
 
-var_dump($order1);
+// je créé un objet
+
+
+// je créé une instance de la classe Order
+// c'est à dire un objet issu du plan de construction de la classe Order
+$order1 = new Order("Anthony Gevers"); //On créé un nouvel objet Order avec le nom du client et un identifiant unique (uniqid)
+$order1->addProduct();
+$order1->addProduct();
+$order1->addProduct();
+
+$order2 = new Order();
+$order2->addProduct();
+
+
+$order1->pay();;
